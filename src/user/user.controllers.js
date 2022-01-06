9bee478c1011cd9a9e0372a77caa469173647ae8
Andrew Bolton <boltonandrew@live.co.uk>
2022-01-06 11:27:03 +0000

    jwt's

--- src/user/user.controllers.js
+++ src/user/user.controllers.js
@@ -1,9 +1,11 @@
 const User = require("./user.model");
+const jwt = require("jsonwebtoken");
 
 exports.addUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).send({ message: "Success", user });
+    const token = await user.generateAuthToken();
+    res.status(201).send({ message: "Success", user, token });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Check server logs" });
@@ -14,6 +16,16 @@ exports.listUser = async (req, res) => {
   try {
     const user = await User.findOne({ username: req.params.username });
     res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Check server logs" });
+  }
+};
+
+exports.logIn = async (req, res) => {
+  try {
+    const token = await req.user.generateAuthToken();
+    res.status(200).send({ user: req.user.username, token });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Check server logs" });
@@ -38,6 +50,18 @@ exports.deleteUser = async (req, res) => {
   try {
     const deletedUser = await User.deleteOne({ username: req.params.username });
     res.status(200).send({ message: "Success", deletedUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Check server logs" });
+  }
+};
+
+exports.tokenCheck = async (req, res) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    const user = await User.findById(decodedToken._id);
+    res.status(200).send({ username: user.username });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Check server logs" });
